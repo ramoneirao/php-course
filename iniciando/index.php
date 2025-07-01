7398b2240f6a11c5d673040d9219c6f629369f7b
Ramon Mendes <ramonneirao@gmail.com>
2025-07-01 12:28:28 -0300

    Hello, World!

---
+++ iniciando/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo de PHP</title>
+</head>
+<body>
+    <!-- <button> Botão </button> -->
+</body>
+</html>
+
+<?php
+    echo "Hello, World!<br>";
+?>
+
